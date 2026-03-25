31b39949d00f87a650ea06f682838a2b70b90e40
James Carl Junio <jamescarljunio@gmail.com>
2026-03-26 00:05:35 +0800

    feat: initialize fastapi app

--- stellar/main.py
+++ stellar/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from stellar-backend!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
